c47bffc8d7c36697ca78a4d4ffdee5304e33db11
anh.nguyen <dev@ezcloud.vn>
2025-06-15 19:45:56 +0700

    Chỉnh giao diện

---
+++ admin-template/src/App_.vue
@@ -0,0 +1,9 @@
+<template>
+  <AppLayout>
+    <div class="text-2xl font-bold">🏠 Dashboard content goes here</div>
+  </AppLayout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from './layouts/main/AppLayout.vue';
+</script>
